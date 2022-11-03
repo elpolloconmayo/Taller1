896db80eba712b68122a809f522db04532a14c60
Gianfranco_Pallero <gpallero@estudiantesunap.cl>
2022-11-03 17:48:59 -0300

    diseño inicial de registro e ingreso

--- codigo.rb
+++ codigo.rb
@@ -1,3 +1,35 @@
+def Ingresar()
+    puts "Ingrese su nombre de usuario: "
+    nombre = gets.chomp
+    puts "Ingrese su contraseña: "
+    contraseña = gets.chomp
+    if false #aqui va conexion con BDD
+        puts "Bienvenido #{nombre}"
+        Main(Usuario)
+    else
+        puts "Usuario o contraseña incorrectos"
+    end
+end
+
+def Registrarse()
+    puts "Ingrese su nombre"
+    nombre = gets.chomp
+    puts "Ingrese su primer apellido"
+    apellido = gets.chomp
+    puts "Ingrese su segundo apellido"
+    apellido2 = gets.chomp
+    puts "Ingrese su correo"
+    edad = gets.chomp.to_i
+    puts "Ingrese su contraseña"
+    contraseña = gets.chomp
+    if false #aqui va conexion con BDD
+        puts "Usuario registrado" 
+        Main(Usuario)
+    else
+        puts "Error al registrar usuario, favor de intentar de nuevo"
+    end
+end
+
 res = true
 while res == true
     puts "1.- Ingresar"
