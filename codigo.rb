bcfceae2d20c8ec6ca9f9f2651ea1216720f63aa
Gianfranco_Pallero <gpallero@estudiantesunap.cl>
2022-11-03 13:22:04 -0300

    Pregunta 2 con sus respectivas opciones

--- codigo.rb
+++ codigo.rb
@@ -1,18 +1,24 @@
 def Question1(Usuario)
-    puts "¿Se siente culpable?: "
+    puts "¿Se siente culpable?"
     puts "-No 
     \n-Me culpo a mi mismo, creo decepcionar a la gente.
     \n-Constantemente recuerdo errores pasados.
     \n-El constante sentimiento depresivo es un castigo.
     \n-Oigo voces acusatorias o de denuncia." 
     respuesta = gets.chomp
-    if respuesta == "si"
-        Usuario.puntos = Usuario.puntos + 1
+    Usuario.puntos = Usuario.puntos + respuesta.to_i
     end
 end
 
 def Question2(Usuario)
-    puts "¿ha tenido perdida de peso?: " 
+    puts "¿Tiene insomnio?" 
+    puts "-No, duermo bien.
+    \n-A veces me cuesta dormir, toma mas de media hora lograrlo.
+    \n-Me cuesta dormir cada noche.
+    \n-Constantemente estoy despierto durante la noche y me molesta
+    \n-Estoy despierto durante la noche y puedo levantarme sin problema.
+    \n-Me despierto antes de lo propuesto, pero vuelvo a dormir.
+    \n-No puedo volver a dormir si me levanto de la cama." 
     respuesta = gets.chomp
     if respuesta == "si"
         Usuario.puntos = Usuario.puntos + 1
