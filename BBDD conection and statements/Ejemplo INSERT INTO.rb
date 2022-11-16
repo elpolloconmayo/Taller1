496d16f591f1638873d2389ddd806a721b8fdfde
Gianfranco_Pallero <gpallero@estudiantesunap.cl>
2022-11-16 19:37:51 -0300

    Example of function

--- BBDD conection and statements/Ejemplo INSERT INTO.rb
+++ BBDD conection and statements/Ejemplo INSERT INTO.rb
@@ -2,6 +2,10 @@ require 'PG'
 
 cnxn = PG.connect(host: 'magallanes.inf.unap.cl', dbname: 'gpallero', user: 'gpallero',password: '4Fd3n2hSde')
 
-q = cnxn.prepare('qr1', "INSERT INTO profetionals (run, dv, name, username, pass, email, mother_sname, father_sname) VALUES ( ? , ? , ? , ? , ? , ? , ? , ? )")
+def funcion(run, dv, name, mother_sname, father_sname, cnxn)
+    texto = "INSERT INTO profetionals (run, dv, name, mother_sname, father_sname) VALUES ( #{run} , '#{dv}' , '#{name}' , '#{mother_sname}' , '#{father_sname}' )"
+    q = cnxn.prepare('qr1', texto)
+    cnxn.exec_prepared('qr1')
+end
 
-q.execute('qr1', [10456788, '9', 'David', 'DavidR12', 'david 1 2 3 4', 'davidr@gmail.com','Rojas','Morello'])
+#funcion('10456788', '9', 'David', 'Rojas', 'Morello', cnxn)
