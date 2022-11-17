14593ad230c744308ecf88391c624bc179a6a6e4
Gianfranco_Pallero <gpallero@estudiantesunap.cl>
2022-11-17 00:30:41 -0300

    fixed lenght and names

---
+++ BBDD Surv_sys sql scripts/surv_sys_professionals.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS professionals;
+
+CREATE TABLE professionals (
+  id serial NOT NULL,
+  run int DEFAULT NULL,
+  dv char(1) DEFAULT NULL,
+  name_ varchar(100) NOT NULL,
+  username varchar(100) NOT NULL,
+  pass varchar(100) NOT NULL,
+  email varchar(100) NOT NULL,
+  mother_name varchar(100) NOT NULL,
+  father_name varchar(100) NOT NULL,
+  gender varchar(1) NOT NULL,
+  birthday date NOT NULL,
+  cellphone varchar(12) NOT NULL,
+  deleted_at timestamp with time zone DEFAULT NULL,
+  PRIMARY KEY (id)
+  UNIQUE (id)
+)
--- BBDD Surv_sys sql scripts/surv_sys_surveyeds.sql
+++ BBDD Surv_sys sql scripts/surv_sys_surveyeds.sql
@@ -1,14 +1,17 @@
 DROP TABLE IF EXISTS surveyeds;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE surveyeds (
   id serial NOT NULL,
   run int DEFAULT NULL,
   dv char(1) DEFAULT NULL,
-  name varchar(45) NOT NULL,
+  name_ varchar(45) NOT NULL,
   father_sname varchar(45) NOT NULL,
   mother_sname varchar(45) NOT NULL,
+  gender varchar(1) NOT NULL,
+  birthday date NOT NULL,
+  mail varchar(45),
+  observation varchar(100) DEFAULT NULL,
   deleted_at timestamp with time zone DEFAULT NULL,
   PRIMARY KEY (id),
-  UNIQUE KEY id_UNIQUE (id)
+  UNIQUE(id)
 )
