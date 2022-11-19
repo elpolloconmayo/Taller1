e4934d7b5eb259d5ba0fb346b6326f5b71970702
Gianfranco_Pallero <gpallero@estudiantesunap.cl>
2022-11-19 20:05:16 -0300

    changed questions

--- BBDD Surv_sys sql scripts/surv_sys_questions.sql
+++ BBDD Surv_sys sql scripts/surv_sys_questions.sql
@@ -4,7 +4,7 @@ CREATE TABLE questions (
   id serial NOT NULL,
   name_test varchar(255) DEFAULT NULL,
   questions text NOT NULL,
-  max_point int NOT NULL,
+  n_questions int NOT NULL,
   max_points text NOT NULL,
   description varchar(255) DEFAULT NULL,
   deleted_at timestamp with time zone DEFAULT NULL,
